a339a39c6b304c4d8a098c1c23976c6cc3ee2f8e
Marcos Vinícius <mvnpereira@gmail.com>
2015-12-14 14:15:03 -0300

    Primeiro arquivo comitado

---
+++ erro.php
@@ -0,0 +1,11 @@
+<?php include('header.php'); ?>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1>Página não existe ou não está habilitada no sistema.</h1>
+			</div>
+		</div>
+	</div>
+
+<?php include('footer.php'); ?>
